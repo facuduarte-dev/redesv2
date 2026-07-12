f22c32a48c134d54fe314d5559d296971057e083
Facu Laptop <facuduartedev@gmail.com>
2026-07-12 17:50:06 -0300

    all

---
+++ src/EscanerRed.java
@@ -0,0 +1,19 @@
+public class EscanerRed {
+
+    // Recibir la red a escanear.
+
+    
+
+    // Recorrer las direcciones IP del rango.
+
+    // Construir la IP completa (ej: 192.168.1.24).
+
+    // Convertir el texto a un objeto InetAddress.
+
+    // Verificar si la IP responde al ping.
+
+    // Si responde, crear un objeto Equipo y guardarlo en una lista.
+
+    // Devolver la lista de equipos encontrados.
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] arg){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese la red a escanear: ");
+        String ipIngresada = sc.nextLine();
+
+    }
+
+}
